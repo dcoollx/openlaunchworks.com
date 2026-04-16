e1ec86654d0a77d81e920313ce41fcda00f58ce8
dcoollx <dsqjr@msn.com>
2026-04-15 21:31:43 -0400

    working

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,4 @@
-import { APIGatewayProxyResult, Context, Handler } from 'aws-lambda';
+import {  APIGatewayProxyEventV2, APIGatewayProxyHandlerV2, APIGatewayProxyResult, Context,  } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import {
   DynamoDBDocumentClient,
@@ -9,12 +9,40 @@ import {
 const client = new DynamoDBClient({});
 const dynamodb = DynamoDBDocumentClient.from(client);
 
-type LambdaHttpEvent = {
-  httpMethod: string;
-  path: string;
-  body?: string | null;
-};
-const AddProduct = async (tableName: string, product: { id: string; name: string; price: number }) => {
+
+type ZOHOProduct =  {
+      "Product_Name": string,
+      "id": string,
+      "Unit_Price": number,
+      "Product_Active": boolean,
+      "Product_Code": string,
+      "Modified_Time": string,
+      "$taxable": boolean
+    }
+const getProductImage = async (productId: string, accessToken: string) => {
+  const response = await fetch(`https://zohoapis.com{productId}/photo`, {
+    headers: { 'Authorization': `Zoho-oauthtoken ${accessToken}` }
+  });
+
+  const imageBuffer = await response.arrayBuffer();
+  // You can now upload this buffer to S3 or convert to Base64
+  return Buffer.from(imageBuffer).toString('base64');
+
+}
+const getAccessToken = async () => {
+  const refreshToken = process.env.ZOHO_REFRESH_TOKEN!;
+  const clientId = process.env.ZOHO_CLIENT_ID!;
+  const clientSecret = process.env.ZOHO_CLIENT_SECRET!;
+  const tokenUrl = `https://accounts.zoho.com/oauth/v2/token?refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}&grant_type=refresh_token`;
+    const response: { access_token: string } = await fetch(tokenUrl, {
+      method: 'POST',
+    }).then(res => res.ok ? res : Promise.reject(res))
+    .then(res => res.json() as Promise<{ access_token: string }>);
+    
+    return response.access_token;
+
+}
+const AddProduct = async (tableName: string, product: ZOHOProduct & { photo: string }) => {
       await dynamodb.send(
         new PutCommand({
           TableName: tableName,
@@ -33,13 +61,13 @@ const AddProduct = async (tableName: string, product: { id: string; name: string
     body: JSON.stringify(body),
   });
 
-export const handler: Handler<LambdaHttpEvent, APIGatewayProxyResult> = async (
-  event,
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2,
   context: Context,
 ) => {
   const tableName = process.env.TABLE_NAME!;
-  if(event.path === '/products'){
-    if (event.httpMethod === 'GET') {
+  if(event.rawPath === '/products'){
+    if (event.requestContext.http.method === 'GET') {
       try {
         const products = await getProducts(tableName);
 
@@ -49,7 +77,7 @@ export const handler: Handler<LambdaHttpEvent, APIGatewayProxyResult> = async (
         return response(500, { message: 'Error fetching products' });
       }
     }
-    if (event.httpMethod === 'POST') {
+    if (event.requestContext.http.method === 'POST') {
       if (!event.body) {
         return response(400, { message: 'Missing request body' });
       }
@@ -63,7 +91,7 @@ export const handler: Handler<LambdaHttpEvent, APIGatewayProxyResult> = async (
       }
     }
   }
-  if(event.path === '/contacts' && event.httpMethod === 'POST'){
+  if(event.rawPath === '/contacts' && event.requestContext.http.method === 'POST'){
     if (!event.body) {
       return response(400, { message: 'Missing request body' });
     }
@@ -71,6 +99,30 @@ export const handler: Handler<LambdaHttpEvent, APIGatewayProxyResult> = async (
     console.log('adding a new contact')
 
   }
+  if(event.rawPath === '/update'){
+    //for now i will make this endpoint to trigger a sync with zoho, in the future i want this to be a webhook
+    console.log('syncing with zoho')
+    try{
+    const accessToken = await getAccessToken();
+    const bearerToken = `Zoho-oauthtoken ${accessToken}`;
+    const products = await fetch('https://www.zohoapis.com/crm/v2/Products', {
+      method: 'GET',
+      headers: {
+        Authorization: bearerToken,
+      },
+    }).then(res => res.ok ? res : Promise.reject(res)).then(res => res.json() as Promise<{ data: ZOHOProduct[] }>);
+
+    for(const product of products.data){
+      const photo = await getProductImage(product.id, accessToken);
+      await AddProduct(tableName, { ...product, photo });
+    }
+    return response(200, { message: 'Synced with Zoho successfully' });
+  }catch(error){
+    console.error('Error syncing with Zoho:', error);
+    return response(500, { message: 'Error syncing with Zoho' });
+
+  }
+}
 
   return response(404, { message: 'Not Found' });
   
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -a            # automatically export all variables
-source .env
+source ./.env
 set +a            # disable automatic exporting
+set -e
 # check if Site is set
 if [ -z "$Site" ]; then
   echo "Error: Site variable is not set. Please set the Site variable and try again."
@@ -11,11 +12,16 @@ aws cloudformation package --template-file infra/template.yaml --s3-prefix "$Sit
 aws cloudformation deploy \
   --template-file packaged-template.yaml \
   --stack-name "$Site-stack" \
-  --parameter-overrides UseCustomDomain=false \
+  --parameter-overrides \
+    UseCustomDomain=false  \
+    ZOHOCLIENTID="$ZOHO_CLIENT_ID" \
+    ZOHOCLIENTSECRET="$ZOHO_CLIENT_SECRET" \
+    ZOHOACCESSTOKEN="$ZOHO_ACCESS_TOKEN" \
+    ZOHOREFRESHTOKEN="$ZOHO_REFRESH_TOKEN" \
   --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
 
 # capture lambda function url from stack then update frontend config with it
 export VITE_API_URL=$(aws cloudformation describe-stacks --stack-name "$Site-stack" --query "Stacks[0].Outputs[?OutputKey=='LambdaAPIUrl'].OutputValue" --output text)  
   #upload site to s3 bucket
 cd apps/frontend && npm run build
-aws s3 sync apps/frontend/dist/ s3://"$Site"-stack-hosting-bucket --delete   
+aws s3 sync dist/ s3://"$Site-stack-hosting-bucket" --delete   
